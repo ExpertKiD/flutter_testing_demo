d863430183d140076a97030c1c7a4070729e7533
Suman Adhikari <expertxkid@gmail.com>
2021-12-09 14:38:29 +0545

    Add two methods one gives error and one succeeds

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -9,7 +9,26 @@ class TestScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Test Screen'),
       ),
-      body: Container(),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () async {
+                await methodWithSuccess();
+                await methodWithError();
+              },
+              child: Text('Run Async operation'))),
     );
+  }
+
+  Future methodWithError() async {
+    await Future.delayed(Duration(seconds: 2));
+
+    throw Future.error('Error, I was meant to do this!');
+
+    print('I will never be called!');
+  }
+
+  Future methodWithSuccess() async {
+    await Future.delayed(Duration(seconds: 2));
+    print('Method succeeded');
   }
 }
