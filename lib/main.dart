491c274141cd3b57618f13490272cd569989e0af
Suman Adhikari <expertxkid@gmail.com>
2021-12-13 16:07:57 +0545

    chore: add sentry to capture errors

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,46 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
+import 'package:sentry/sentry.dart';
 
 import 'app/app.dart';
 
-final log = Logger();
+bool isDebugMode() {
+  bool isInDebugMode = false;
+  assert(isInDebugMode == false);
+  return isInDebugMode;
+}
+
+final _log = Logger();
+
+final _sentry = SentryClient(SentryOptions(
+    dsn:
+        'https://a27d08e9edf5482c89aa076073be65b7@o1087080.ingest.sentry.io/6104827'));
 
 Future<void> main() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
 
     FlutterError.onError = (FlutterErrorDetails details) async {
-      if (!kReleaseMode) {
+      if (isDebugMode()) {
         // defaults to dump to console
         FlutterError.dumpErrorToConsole(details);
 
         //or, use logger to dump pretty
-        log.e(details.toString(), details.exception, details.stack);
+        _log.e(details.toString(), details.exception, details.stack);
       } else {
-        log.d('Sending error details to Zone');
+        _log.d('Sending error details to Zone');
         Zone.current.handleUncaughtError(details.exception, details.stack!);
       }
     };
 
     runApp(const MyApp());
   }, (Object error, StackTrace stack) {
-    if (!kReleaseMode) {
-      log.e("Caught Dart Error!", error, stack);
+    if (isDebugMode()) {
+      _log.e("Caught Dart Error!", error, stack);
     } else {
-      log.d("Time to report to error tracking system in production");
+      _log.d("Time to report to error tracking system in production");
+      _sentry.captureException(error, stackTrace: stack);
     }
   });
 }
