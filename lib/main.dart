5918f6d56e1cc832c0afa6239d0628763b874182
Suman Adhikari <expertxkid@gmail.com>
2021-12-13 15:09:02 +0545

    Add onError handler to handle flutter errors

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,19 @@ import 'app/app.dart';
 final log = Logger();
 
 Future<void> main() async {
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (!kReleaseMode) {
+      // defaults to dump to console
+      FlutterError.dumpErrorToConsole(details);
+
+      //or, use logger to dump pretty
+      log.e(details.toString(), details.exception, details.stack);
+    } else {
+      log.d('Sending error details to Zone');
+      Zone.current.handleUncaughtError(details.exception, details.stack!);
+    }
+  };
+
   runZonedGuarded<Future<void>>(() async {
     runApp(const MyApp());
   }, (Object error, StackTrace stack) {
