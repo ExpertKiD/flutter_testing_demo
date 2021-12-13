46464c0d8d0110d03c63de7139f68cba0355e691
Suman Adhikari <expertxkid@gmail.com>
2021-12-13 15:27:53 +0545

    refactor: move onError to inside runZOnedGuarded()

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,22 @@ import 'app/app.dart';
 final log = Logger();
 
 Future<void> main() async {
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    if (!kReleaseMode) {
-      // defaults to dump to console
-      FlutterError.dumpErrorToConsole(details);
-
-      //or, use logger to dump pretty
-      log.e(details.toString(), details.exception, details.stack);
-    } else {
-      log.d('Sending error details to Zone');
-      Zone.current.handleUncaughtError(details.exception, details.stack!);
-    }
-  };
-
   runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    FlutterError.onError = (FlutterErrorDetails details) async {
+      if (!kReleaseMode) {
+        // defaults to dump to console
+        FlutterError.dumpErrorToConsole(details);
+
+        //or, use logger to dump pretty
+        log.e(details.toString(), details.exception, details.stack);
+      } else {
+        log.d('Sending error details to Zone');
+        Zone.current.handleUncaughtError(details.exception, details.stack!);
+      }
+    };
+
     runApp(const MyApp());
   }, (Object error, StackTrace stack) {
     if (!kReleaseMode) {
