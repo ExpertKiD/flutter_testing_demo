a5e0b31f212e0265adcbc500f4a7a13332be0091
Suman Adhikari <expertxkid@gmail.com>
2021-12-14 18:38:02 +0545

    feat: add fake logic to login user with username=username and password=password

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -2,6 +2,11 @@ class LoginService {
   Future<bool> loginWithUsernameAndPassword(
       String username, String password) async {
     await Future.delayed(const Duration(seconds: 2));
-    return Future.value(true);
+
+    if (username == 'username' && password == 'password') {
+      return Future.value(true);
+    } else {
+      return Future.value(false);
+    }
   }
 }
