836e45273f32fd7a4682fc4d6d8f6cd5f8fbaaac
Suman Adhikari <expertxkid@gmail.com>
2021-12-14 17:53:15 +0545

    refactor: add named parameter previously precommitted without check

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -1,7 +1,7 @@
 class LoginService {
   Future<bool> loginWithUsernameAndPassword(
       String username, String password) async {
-    await Future.delayed(Duration(2));
+    await Future.delayed(const Duration(seconds: 2));
     return Future.value(true);
   }
 }
