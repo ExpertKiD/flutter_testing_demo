283fe3c083f22a3cec2fc0c3a1afede1982e8284
Suman Adhikari <expertxkid@gmail.com>
2021-12-14 19:43:53 +0545

    chore: add makeTestableWidget() function to create the base test widget

--- test/widget_tests/screens/test_test.dart
+++ test/widget_tests/screens/test_test.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testingdemo/services/login_service.dart';
 
 void main() {
+  Widget makeTestableWidget(Widget child, LoginService loginService) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+
   group('Test screen test', () {
     group('Login widget tests', () {
       // Setup mock dependencies before each test
@@ -12,7 +20,7 @@ void main() {
         // clear mock here
       });
 
-      testWidgets('description', (WidgetTester tester) async {
+      testWidgets('should render', (WidgetTester tester) async {
         // Arrange
 
         // Act
