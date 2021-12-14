c32b7c8d70ed524dbf6f1921557186740d70443b
Suman Adhikari <expertxkid@gmail.com>
2021-12-14 19:29:05 +0545

    chore: add test screen's tests in test/screens

---
+++ test/screens/test_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Test screen test', () {
+    group('Login widget tests', () {
+      // Setup mock dependencies before each test
+      setUp(() {
+        // Mock here
+      });
+
+      tearDown(() {
+        // clear mock here
+      });
+
+      testWidgets('description', (WidgetTester tester) async {
+        // Arrange
+
+        // Act
+
+        // Assert
+      });
+    });
+  });
+}
