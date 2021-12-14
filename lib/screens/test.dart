60f3f03c95145d032a18ccdabf9a7cb2edaa3e64
Suman Adhikari <expertxkid@gmail.com>
2021-12-14 21:46:22 +0545

    chore: add all widget in test screen inside a scroll view

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -18,92 +18,94 @@ class TestScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Test Screen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 16.0, right: 16, top: 16),
-        child: Column(
-          children: [
-            TextField(
-              decoration:
-                  InputDecoration(hintText: 'Click me! to cause layout error'),
-            ),
-            Center(
-              child: ElevatedButton(
-                  onPressed: () async {
-                    await methodWithSuccess();
-                    await methodWithError();
-                  },
-                  child: Text('Run Async operation')),
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: 'Username'),
-              key: Key('txt_username'),
-              controller: usernameController,
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: 'Password'),
-              key: Key('txt_password'),
-              controller: passwordController,
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  final username = usernameController.text;
-                  final password = passwordController.text;
-
-                  if (await loginService.loginWithUsernameAndPassword(
-                      username, password)) {
-                    showDialog(
-                      context: context,
-                      builder: (ctx) {
-                        return AlertDialog(
-                          key: const Key('alert_login_success'),
-                          title: const Text(
-                              'You have been logged in successfully'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(ctx).pop();
-                              },
-                              child: const Text('Ok'),
-                            )
-                          ],
-                        );
-                      },
-                    );
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text('Falied to login'),
-                      action: SnackBarAction(
-                        key: Key('snack_login_failed'),
-                        onPressed: () {},
-                        label: 'Retry',
-                      ),
-                    ));
-                  }
-                },
-                child: Text('Login'),
-                key: Key('btn_login'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0, right: 16, top: 16),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Click me! to cause layout error'),
               ),
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: index % 2 == 0
-                        ? Colors.blue.shade400
-                        : Colors.grey.shade400,
-                  ),
-                  child: Center(
-                      child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Text('Item #${index + 1}'),
-                  )),
-                );
-              },
-              itemCount: 8,
-            )
-          ],
+              Center(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      await methodWithSuccess();
+                      await methodWithError();
+                    },
+                    child: Text('Run Async operation')),
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: 'Username'),
+                key: Key('txt_username'),
+                controller: usernameController,
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: 'Password'),
+                key: Key('txt_password'),
+                controller: passwordController,
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final username = usernameController.text;
+                    final password = passwordController.text;
+
+                    if (await loginService.loginWithUsernameAndPassword(
+                        username, password)) {
+                      showDialog(
+                        context: context,
+                        builder: (ctx) {
+                          return AlertDialog(
+                            key: const Key('alert_login_success'),
+                            title: const Text(
+                                'You have been logged in successfully'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(ctx).pop();
+                                },
+                                child: const Text('Ok'),
+                              )
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: Text('Falied to login'),
+                        action: SnackBarAction(
+                          key: Key('snack_login_failed'),
+                          onPressed: () {},
+                          label: 'Retry',
+                        ),
+                      ));
+                    }
+                  },
+                  child: Text('Login'),
+                  key: Key('btn_login'),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: index % 2 == 0
+                          ? Colors.blue.shade400
+                          : Colors.grey.shade400,
+                    ),
+                    child: Center(
+                        child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Text('Item #${index + 1}'),
+                    )),
+                  );
+                },
+                itemCount: 8,
+              )
+            ],
+          ),
         ),
       ),
     );
