288f5a88723c948a9d2c3c4eff1aa86e3bc3812f
Suman Adhikari <expertxkid@gmail.com>
2021-12-15 13:09:29 +0545

    chore: add verification calls to verify whether loginService is called or not during widget test

--- test/widget_tests/screens/test_test.dart
+++ test/widget_tests/screens/test_test.dart
@@ -22,11 +22,12 @@ void main() {
         // Mock here
         loginService = MockLoginService();
 
+        when(() => loginService.loginWithUsernameAndPassword(any(), any()))
+            .thenAnswer((invocation) => Future.value(false));
+
         when(() => loginService.loginWithUsernameAndPassword(
                 'username', 'password'))
             .thenAnswer((invocation) => Future.value(true));
-        when(() => loginService.loginWithUsernameAndPassword(any(), any()))
-            .thenAnswer((invocation) => Future.value(false));
       });
 
       tearDown(() {
@@ -45,6 +46,63 @@ void main() {
 
         expect(find.byType(TestScreen), findsOneWidget);
         // Assert
+      });
+
+      testWidgets('user should login with correct username and password',
+          (WidgetTester tester) async {
+        // Arrange
+        TestScreen testScreen = TestScreen(loginService: loginService);
+
+        verifyNever(() =>
+            loginService.loginWithUsernameAndPassword('username', 'password'));
+
+        // Act
+        await tester.pumpWidget(
+          makeTestableWidget(testScreen),
+        );
+
+        await tester.enterText(find.byKey(Key('txt_username')), 'username');
+        await tester.enterText(find.byKey(Key('txt_password')), 'password');
+
+        await tester.tap(find.byKey(Key('btn_login')));
+
+        verify(() => loginService.loginWithUsernameAndPassword(
+            'username', 'password')).called(1);
+
+        await tester.pump();
+
+        // Assert
+        expect(find.byKey(Key('alert_login_success')), findsOneWidget);
+      });
+
+      testWidgets('user should fail at login with wrong username and password',
+          (WidgetTester tester) async {
+        // Arrange
+        TestScreen testScreen = TestScreen(loginService: loginService);
+
+        verifyNever(() => loginService.loginWithUsernameAndPassword(
+            'wrong-username', 'wrong-password'));
+
+        // Act
+        await tester.pumpWidget(
+          makeTestableWidget(testScreen),
+        );
+
+        await tester.enterText(
+            find.byKey(Key('txt_username')), 'wrong-username');
+        await tester.enterText(
+            find.byKey(Key('txt_password')), 'wrong-password');
+
+        await tester.tap(find.byKey(Key('btn_login')));
+
+        verify(() => loginService.loginWithUsernameAndPassword(
+            'wrong-username', 'wrong-password')).called(1);
+
+        await tester.pump();
+
+        // Assert
+        expect(find.byKey(Key('alert_login_success')), findsNothing);
+        expect(find.byKey(Key('snack_login_failed')), findsOneWidget);
       });
     });
   });
